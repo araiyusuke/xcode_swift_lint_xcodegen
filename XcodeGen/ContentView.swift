75ae3a62ba93f042be66b3748e4a3b199c2f3011
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-13 07:38:30 +0900

    remove: 不要なプロジェクトファイル

--- XcodeGen/ContentView.swift
+++ XcodeGen/ContentView.swift
@@ -9,28 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack(spacing: 0) {
-                Color.red.frame(width: 68, height: 68)
-                    .padding(.trailing, 32)
-                Text("とてつもなく長い名前のモンスター")
-                    .padding(.trailing, 32)
-                    .background(Color.gray)
-            }
-            .padding(16)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .background(Color.blue)
-
-            HStack(spacing: 0) {
-                Color.red.frame(width: 68, height: 68)
-                    .padding(.trailing, 32)
-                Text("とてつもなく")
-                    .padding(.trailing, 32)
-                    .background(Color.gray)
-            }
-            .padding(16)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .background(Color.blue)
-        }
+       Text("ContentView")
     }
 }
